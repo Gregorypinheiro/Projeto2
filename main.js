ec91973180a91e06966dc61dcc9994666d8f4e42
gregory <gregorypinheiro88@gmail.com>
2024-04-05 18:19:26 -0300

    fim do dia

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
 const form = document.getElementById('form-atividade');
-form.addEventListener('submit', function(e) {
+const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji celebrando" />';
+const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji decepcionado" />';
+
+let linhas = ' ';
+
+form.addEventListener('submit', function(e){
     e.preventDefault();
 
-    const inputNomeAtividade = ducument.getElementById('nome-atividade');
-    const inputNotaAtividade = ducument.getElementById('nota-atividade');
+    const inputNomeAtividade = document.getElementById('nome-atividade');
+    const inputNotaAtividade = document.getElementById('nota-atividade');
 
-    alert(`Atividade: ${inputNomeAtividade.value} - Nota: ${inputNotaAtividade.value}`);
-});
+
+    let linha = '<tr>';
+    linha += `<td>${inputNomeAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}</td>`;
+    linha += '</tr>';
+
+    linhas += linha;
+
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+
+    inputNomeAtividade.value = '';
+    inputNotaAtividade.value = '';
+})
